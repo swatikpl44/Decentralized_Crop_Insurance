bbd21b3ec942253bbc7065ffa79fff6afce32a1f
Swatik <swatikpl44@gmail.com>
2021-09-22 12:59:41 +0530

    Setup Project Structure using Truffle-react

---
+++ truffle-config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  networks: {
+    develop: {
+      port: 8545,
+    },
+    development: {
+      host: "localhost",
+      port: 7545,
+      network_id: "5777",
+    },
+  },
+};
